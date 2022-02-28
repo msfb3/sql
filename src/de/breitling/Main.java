af1615ef658c4acb89e9586927d6b30bdc2d1b7a
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2022-02-28 16:46:48 +0100

    Add the first query

--- src/de/breitling/Main.java
+++ src/de/breitling/Main.java
@@ -3,6 +3,8 @@ package de.breitling;
 import org.mariadb.jdbc.Connection;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
@@ -12,6 +14,13 @@ public class Main {
             Connection connection =(Connection) DriverManager.getConnection("jdbc:mariadb://localhost:3306/chinook", "root", "");
 
             System.out.println("Connection established.");
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM customer LIMIT 10");
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getString("FirstName") + ""  + rs.getString("Company"));
+            }
+            connection.close();
 
         }catch (SQLException sqlEx) {
             System.out.println("Something went wrong while establishing the Connection " + sqlEx.getMessage());
